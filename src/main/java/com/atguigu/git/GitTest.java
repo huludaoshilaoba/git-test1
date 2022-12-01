9d4d780170935d34166379f91ef3ca66ffebb58d
wangzhenyu <1164023848@qq.com>
2022-12-01 10:25:15 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是你哥哥");
+        System.out.println("master test");
     }
 }
