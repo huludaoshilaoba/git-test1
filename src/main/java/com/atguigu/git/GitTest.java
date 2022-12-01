b615db1683ac9da674c8dd344a7dbe340f9f12d7
wangzhenyu <1164023848@qq.com>
2022-12-01 10:24:02 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是你哥哥");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
