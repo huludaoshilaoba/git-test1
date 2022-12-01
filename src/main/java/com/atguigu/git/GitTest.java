1ad9661e3c750c3f7685f9b507829576f70443d5
huludaoshilaoba <107481377+huludaoshilaoba@users.noreply.github.com>
2022-12-01 11:37:13 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hot-fix2");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("Gitee test");
     }
 }
